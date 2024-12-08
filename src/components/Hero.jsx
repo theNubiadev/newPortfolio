261b39745f1c4a4b2275b6baca9bb7aa1d62ff3a
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-12-08 15:13:11 +0100

    d

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ function Hero() {
 
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden ">
-            <img src={her} alt="" width={656} height={800} />
+            <img src={} alt="" width={656} height={800} />
           </figure>
         </div>
       </div>
